b3f3ce8fb91789d8fb69c64db9549f023473f148
VittalVelturi <vittal.velturi@gmail.com>
2025-01-30 13:47:17 +0530

    Updated the output.tf file

--- Creation of instance with variable and outputs/output.tf
+++ Creation of instance with variable and outputs/output.tf
@@ -1,4 +1,37 @@
 #Provides all the attribute of an ec2 instance
-output "ec2_Instance_attributes" {
-  value = aws_instance.ec2_Hardcoding
+output "ami" {
+  value = aws_instance.ec2_Hardcoding.ami
+}
+output "arn" {
+  value = aws_instance.ec2_Hardcoding.arn
+}
+output "instance_state" {
+  value = aws_instance.ec2_Hardcoding.instance_state
+}
+output "primary_network_interface_id" {
+  value = aws_instance.ec2_Hardcoding.primary_network_interface_id
+}
+output "private_ip" {
+  value = aws_instance.ec2_Hardcoding.private_ip
+}
+output "private_dns" {
+  value = aws_instance.ec2_Hardcoding.private_dns
+}
+output "public_dns" {
+  value = aws_instance.ec2_Hardcoding.public_dns
+}
+output "public_ip" {
+  value = aws_instance.ec2_Hardcoding.public_ip
+}
+output "EBS_Volume_id" {
+  value = aws_instance.ec2_Hardcoding.root_block_device[0].volume_id
+}
+output "EBS_Volume_Size" {
+  value = aws_instance.ec2_Hardcoding.root_block_device[0].volume_size
+}
+output "EBS_Volume_type" {
+  value = aws_instance.ec2_Hardcoding.root_block_device[0].volume_type
+}
+output "EBS_Volume_Device" {
+  value = aws_instance.ec2_Hardcoding.root_block_device[0].device_name
 }
